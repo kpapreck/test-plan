94457fb6750906fa6ad379c23c2bed119e445fe4
Kevin Papreck <kevin@kevins-mbp-2.lan>
2018-01-29 20:34:28 -0600

    1

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 docker build -t solidfire-ps-powershell-build1 .
-docker run -it -v $(pwd)/scripts:/scripts --rm solidfire-ps-powershell-build1 bash
+docker run -it -v $(pwd):/scripts --rm solidfire-ps-powershell-build1 bash
