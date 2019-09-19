7328caff773b9cc0bf1ef395c4bea109ab13bc56
Kevin Papreck <kevin@kpapreck-mac-0.lan>
2019-09-19 07:01:59 -0500

    1

--- change-management-vlan.sh
+++ change-management-vlan.sh
@@ -7,7 +7,7 @@ sed 's/vlan 2541-2543/vlan 2542-2543/' 2-cable-HCIow-sw-a-DNS-DHCP.txt > 2-cable
 sed 's/2541/'$mvlan'/g' 2-cable-HCIow-sw-a-DNS-DHCP-mod.txt > 2-cable-HCIow-sw-a-DNS-DHCP-mod2.txt
 sed '1,/vlan 4000/ {/vlan 4000/a\
 vlan '$mvlan'
-}' 2-cable-HCIow-sw-a-DNS-DHCP-mod2.txt > 2-cable-HCIow-sw-a-DNS-DHCP-modified.txt 
+}' 2-cable-HCIow-sw-a-DNS-DHCP-mod2.txt > hciow-switch-a-vlan-$mvlan.txt 
 rm 2-cable-HCIow-sw-a-DNS-DHCP-mod.txt
 rm 2-cable-HCIow-sw-a-DNS-DHCP-mod2.txt
 
@@ -16,6 +16,6 @@ sed 's/vlan 2541-2543/vlan 2542-2543/' 2-cable-HCIow-sw-b-DNS-DHCP.txt > 2-cable
 sed 's/2541/'$mvlan'/g' 2-cable-HCIow-sw-b-DNS-DHCP-mod.txt > 2-cable-HCIow-sw-b-DNS-DHCP-mod2.txt
 sed '1,/vlan 4000/ {/vlan 4000/a\
 vlan '$mvlan'
-}' 2-cable-HCIow-sw-b-DNS-DHCP-mod2.txt > 2-cable-HCIow-sw-b-DNS-DHCP-modified.txt
+}' 2-cable-HCIow-sw-b-DNS-DHCP-mod2.txt > hciow-switch-b-vlan-$mvlan.txt
 rm 2-cable-HCIow-sw-b-DNS-DHCP-mod.txt
 rm 2-cable-HCIow-sw-b-DNS-DHCP-mod2.txt
